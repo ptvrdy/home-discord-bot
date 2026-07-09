b65c1a9c8f1b0e6855c6549041698e6be111a5a1
Peyton Tvrdy <peyton.tvrdy123@gmail.com>
2026-07-09 12:27:02 -0400

    fixed recipe variable

--- services/scraper.py
+++ services/scraper.py
@@ -14,7 +14,7 @@ def safe_scrape(method):
 def scrape_recipe(url: str):
     scraper = scrape_me(url)
 
-    return Recipe(
+    recipe = Recipe(
         title=safe_scrape(scraper.title) or "Untitled Recipe",
         ingredients=safe_scrape(scraper.ingredients) or [],
         instructions=safe_scrape(scraper.instructions),
