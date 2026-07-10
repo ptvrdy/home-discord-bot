1cc16365b2118175ca89dc4923ba87d9a41b6ff0
Peyton Tvrdy <peyton.tvrdy123@gmail.com>
2026-07-10 16:28:27 -0400

    refactored based on discord tags

--- services/recipe_tags.py
+++ services/recipe_tags.py
@@ -1,27 +1,59 @@
 from services.time_parser import parse_minutes
 
+def contains_main_ingredient(
+    ingredients,
+    ingredient_name
+):
+    exclusions = {
+        "chicken": [
+            "chicken broth",
+            "chicken stock",
+            "chicken bouillon",
+        ]
+    }
+
+    for item in ingredients:
+
+        if ingredient_name in item:
+
+            for excluded in exclusions.get(
+                ingredient_name,
+                []
+            ):
+                if excluded in item:
+                    return False
+
+            return True
+
+    return False
 
 def generate_recipe_tags(recipe):
     tags = []
 
-    searchable_text = (
-        recipe.title.lower() +
-        " " +
-        " ".join(recipe.ingredients).lower()
-    )
-    
-    total_minutes = parse_minutes(recipe.total_time)
+    title = recipe.title.lower()
 
-    if "chicken" in searchable_text:
+    ingredients = [
+        ingredient.lower()
+        for ingredient in recipe.ingredients
+    ]
+    
+    ingredient_text = " ".join(ingredients)
+    
+    if contains_main_ingredient(
+        ingredients,
+        "chicken"
+    ):
         tags.append("chicken")
 
-    if "soup" in searchable_text:
+    if "soup" in title or "soup" in ingredient_text:
         tags.append("soup")
 
     if recipe.total_minutes is not None:
         if recipe.total_minutes <= 30:
             tags.append("quick")
+
         if recipe.total_minutes >= 90:
             tags.append("long")
 
+    
     return tags
